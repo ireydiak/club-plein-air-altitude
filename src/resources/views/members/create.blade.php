def6530712074e94131f35cf1c46193d4af16f87
ireydiak <ireydiak@gmail.com>
2020-01-12 19:02:03 -0500

    [FEATURE] Members: Formulaire create.

---
+++ src/resources/views/members/create.blade.php
@@ -0,0 +1,29 @@
+<form>
+    <div>
+        <label>Prénom</label>
+        <input type="text" name="firstName" />
+    </div>
+    <div>
+        <label>Nom</label>
+        <input type="text" name="lastName" />
+    </div>
+    <div>
+        <label for="">Email</label>
+        <input type="text" name="email">
+    </div>
+    <div>
+        <label>Password</label>
+        <input type="password" name="password" />
+    </div>
+    <div>
+        <label>CIP</label>
+        <input type="text" name="universityId" />
+    </div>
+    <div>
+        <label for="">Facebook</label>
+        <input type="text" name="facebookLink">
+    </div>
+    <div>
+        <input type="submit" value="Créer">
+    </div>
+</form>
